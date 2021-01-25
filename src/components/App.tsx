1352a662d0c6aac7c7fabd178013cc639ee3407f
HongJunHyeok <edb1631@naver.com>
2021-01-25 23:09:58 +0900

    [ADD] starbucks module

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,35 +2,22 @@ import { Caption1, GridList, Subtitle1 } from '@class101/ui';
 import React from 'react';
 
 function App() {
-  return (
-    <>
-      <GridList
-        items={[
-          {
-            title: '제주 비자림 콜드 브루',
-            discount: 10,
-          },
-          {
-            title: '콜드 브루',
-            discount: 40,
-          },
-        ]}
-        renderItem={(item) => {
-          const { title, discount } = item;
-          return (
-            <div>
-              <Subtitle1>{title}</Subtitle1>
-              <Caption1 fontWeight="Bold" color="green">
-                {discount}% 할인
-              </Caption1>
-            </div>
-          );
-        }}
-        smColumn={1}
-        lgColumn={3}
-      ></GridList>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
+
+// [
+//   {
+//     product_name: '블랙 그레이즈드 라떼',
+//     id: 1,
+//     img: 'assets/starbucks.png',
+//     price: '6.1',
+//   },
+//   {
+//     product_name: '스타벅스 몰체 라떼',
+//     id: 2,
+//     img: 'assets/starbucks.png',
+//     price: '5.6',
+//   },
+// ]
---
+++ src/modules/starbucks.tsx
@@ -0,0 +1,50 @@
+//action type
+const ADD_CART = 'starbucks/ADD_CART' as const;
+const REMOVE_CART = 'starbucks/REMOVE_CART' as const;
+
+//action creator
+export const addCart = (id: number, product_name: string, price: string) => ({
+  type: ADD_CART,
+  payload: [
+    {
+      id,
+      product_name,
+      price,
+    },
+  ],
+});
+
+export const removeCart = (id: number) => ({
+  type: REMOVE_CART,
+  payload: id,
+});
+
+export type StarbucksActions =
+  | ReturnType<typeof addCart>
+  | ReturnType<typeof removeCart>;
+
+//state
+export type StarbucksItem = {
+  product_name: string;
+  id: number;
+  price: string;
+};
+
+export type StarbucksItems = StarbucksItem[];
+
+const initialState: StarbucksItems = [];
+
+//reducer
+export default function starbucks(
+  state: StarbucksItems = initialState,
+  action: StarbucksActions
+) {
+  switch (action.type) {
+    case ADD_CART:
+      return state.concat(...state, action.payload);
+    case REMOVE_CART:
+      return state.filter((item) => item.id !== action.payload);
+    default:
+      return state;
+  }
+}
